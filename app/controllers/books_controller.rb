a0989591f3ebceda967e08e1da8b42366a579ca2
wkn816 <wkn816@gmail.com>
2020-10-14 17:29:34 +0000

    add_validation

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,15 +2,17 @@ class BooksController < ApplicationController
 
 
   def create
-    book = Book.new(book_params)
-    book.user_id = current_user.id
-   if book.save
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+   if @book.save
       redirect_to books_path
    else
+     @books = Book.all
+     @user = current_user
       render :index
-   end 
+   end
   end
-  
+
 
   def index
     @book = Book.new
@@ -24,23 +26,23 @@ class BooksController < ApplicationController
     @user = current_user
 
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book)
   end
-  
+
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
